690c619bd216ae91abdbb4ad37fdfd9fd7590669
Anıl Burak Dolaşık <anilburakd05@gmail.com>
2024-11-01 21:57:55 +0300

    Some issues solved, some still exist

--- Hangman_Game/game.py
+++ Hangman_Game/game.py
@@ -51,30 +51,44 @@ hangman = ['''
 
 kelime = input("Kelimeyi girin: ")
 uzunluk = len(kelime)
+kelimedekiHarfler = list(kelime)
+dogruHarfler = []
 kullanilan_harfler = []
 sayac = 0
 while (True):
     for element in kelime:
         if element in kullanilan_harfler:
-            print(element, end="")
+            print(element, end=" ")
         elif element == " ":
-            print("/", end="")
+            print("/ ", end="")
         else:
-            print("_", end="")
-    newLetter = input(" ")
-    kullanilan_harfler.append(newLetter)
-    if newLetter not in kelime:
-        sayac += 1
-        if (sayac < 7):
-            print(hangman[sayac])
-            print("Bulunmayan harfler: ", end="")
-            for letter in kullanilan_harfler:
-                if letter not in kelime:
-                    print(letter, end=" ")
-            print("")
-        if sayac == 7:
-            print("Hak bitti")
-            break
-
+            print("_ ", end="")
+    yeniHarf = input(" ")
+    if dogruHarfler == kelimedekiHarfler:
+        print(kelime)
+        print("Kazandınız.")
+    if yeniHarf in kullanilan_harfler:
+        print("Harf daha önce girildi")
+    else:
+        kullanilan_harfler.append(yeniHarf)
+        if yeniHarf not in kelime:
+            sayac += 1
+            if (sayac < 6):
+                print(hangman[sayac])
+                print("Bulunmayan harfler: ", end="")
+                for letter in kullanilan_harfler:
+                    if letter not in kelime:
+                        print(letter, end=" ")
+                print("")
+            if sayac == 6:
+                print(hangman[6])
+                print("Hak bitti")
+                break
+        else:
+            dogruHarfler.append(yeniHarf)
+            if dogruHarfler == kelimedekiHarfler:
+                print(kelime)
+                print("Kazandınız.")
+                break
 
 
