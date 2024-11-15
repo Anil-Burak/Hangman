3b77d785f62aad791478d71a10cde7cdf001af87
Anıl Burak Dolaşık <anilburakd05@gmail.com>
2024-11-16 01:01:16 +0300

    Works properly now

--- Hangman_Game/game.py
+++ Hangman_Game/game.py
@@ -1,4 +1,5 @@
-hangman = ['''
+hangman = [" ",
+           '''
   +---+
   |   |
       |
@@ -49,13 +50,26 @@ hangman = ['''
       |
 =========''']
 
+
+def harf_kontrol(character):
+    if len(character) == 1 and character.isalpha():
+        return True
+    else:
+        print("Doğru şekilde harf girmediniz.")
+        return False
+
+
 word = input("Kelimeyi girin: ")
-wordLength = len(word)
-lettersInWord = list(word)
+wordList = list(word)
+lettersInWord = []
+for letter in wordList:
+    if letter != " " and letter not in lettersInWord:
+        lettersInWord.append(letter)
+lettersInWord.sort()
 correctLetters = []
 usedLetters = []
 count = 0
-while (True):
+while True:
     for element in word:
         if element in usedLetters:
             print(element, end=" ")
@@ -64,31 +78,30 @@ while (True):
         else:
             print("_ ", end="")
     newLetter = input(" ")
-    if correctLetters == lettersInWord:
-        print(word)
-        print("Kazandınız.")
-    if newLetter in usedLetters:
-        print("Harf daha önce girildi")
-    else:
-        usedLetters.append(newLetter)
-        if newLetter not in word:
-            count += 1
-            if (count < 6):
-                print(hangman[count])
-                print("Bulunmayan harfler: ", end="")
-                for letter in usedLetters:
-                    if letter not in word:
-                        print(letter, end=" ")
-                print("")
-            if count == 6:
-                print(hangman[6])
-                print("Hak bitti")
-                break
-        else:
+    if harf_kontrol(newLetter):
+        if newLetter in usedLetters:
+            print("Harf önceden girilmiş")
+            continue
+        elif newLetter in lettersInWord:
             correctLetters.append(newLetter)
+            correctLetters.sort()
             if correctLetters == lettersInWord:
                 print(word)
-                print("Kazandınız.")
+                print("Kazandınız")
                 break
-
-
+            usedLetters.append(newLetter)
+        else:
+            usedLetters.append(newLetter)
+            count += 1
+            print(hangman[count])
+            if count == 7:
+                print("Kaybettiniz.")
+                break
+            else:
+                print("Yanlış harfler: ", end="")
+                for x in usedLetters:
+                    if x in correctLetters:
+                        continue
+                    else:
+                        print(x, end=" ")
+                print("")
